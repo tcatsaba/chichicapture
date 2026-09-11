a14adcf71cfd13902befb8110db7b42a583f76f9
tcatsaba <djmumu@naver.com>
2026-09-12 02:07:26 +0900

    Resolve shared WPF and WinForms type names

---
+++ ChichiCapture/GlobalUsings.cs
@@ -0,0 +1,7 @@
+global using System.IO;
+global using Application = System.Windows.Application;
+global using Clipboard = System.Windows.Clipboard;
+global using MessageBox = System.Windows.MessageBox;
+global using SaveFileDialog = Microsoft.Win32.SaveFileDialog;
+global using PixelFormat = System.Drawing.Imaging.PixelFormat;
+global using Path = System.IO.Path;
