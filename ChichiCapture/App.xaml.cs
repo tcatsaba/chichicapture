1ac6c28ea4b39e9cccf8e1b5b8cd02dc1bafc187
tcatsaba <djmumu@naver.com>
2026-09-12 02:04:00 +0900

    Fix WPF type ambiguity in ChichiCapture/App.xaml.cs

--- ChichiCapture/App.xaml.cs
+++ ChichiCapture/App.xaml.cs
@@ -1,6 +1,6 @@
 using System.Windows;
 namespace ChichiCapture;
-public partial class App : Application
+public partial class App : System.Windows.Application
 {
  protected override void OnStartup(StartupEventArgs e){base.OnStartup(e);new MainWindow().Show();}
 }
